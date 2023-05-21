fcbe7797a4a2d337671662990c2114d359adfed1
thadeus <thadeusnyariki94@gmail.com>
2023-05-21 17:34:12 +0300

    first commit

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo "Hello world"
+
+#COMMENTS
+#this is a comment
+echo "Hello world" #this is also a comment
+
+#VARIABLES
+# 1. system variables
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
